d2abd3aed173a16a13f8e79c95324a7ca4221e80
singularitti <singularitti@outlook.com>
2026-03-26 05:11:57 -0500

    Update tests/test_searcher.py

--- tests/test_searcher.py
+++ tests/test_searcher.py
@@ -1,5 +1,6 @@
-from typing import Any
+import warnings
 import weakref
+from typing import Any
 
 
 def test_search_and_relax(monkeypatch):
@@ -24,6 +25,11 @@ def test_search_and_relax(monkeypatch):
 
     from mp_helper.materials import MaterialsSearcher
 
+    monkeypatch.setattr(
+        "mp_helper.materials.get_relax_sets",
+        lambda records: [type("DummyRelaxSet", (), {"struct": records[0]})()],
+    )
+
     # default client creation
     s = MaterialsSearcher()
     results = s.search(chemsys="X")
@@ -45,7 +51,7 @@ def test_search_and_relax(monkeypatch):
     # context manager should close owned client
     s3 = MaterialsSearcher()
     with s3 as ctx:
-        _ = ctx.search(foo="bar")
+        _ = ctx.search(chemsys="X")
     # after exiting, owned client should have been closed
     assert getattr(s3._mpr, "closed", False) is True
 
@@ -70,3 +76,64 @@ def test_owned_client_closed(monkeypatch):
     gc.collect()
     # we don't have access to the DummyClient instance here, but ensure ref is dead
     assert ref() is None
+
+
+def test_summary_searcher_chunks_and_warning(monkeypatch):
+    class DummySummary:
+        def __init__(self):
+            self.queries: list[Any] = []
+            self.pages = [
+                [{"material_id": "mp-1"}],
+                [{"material_id": "mp-2"}],
+                [],
+            ]
+
+        def _query_resource(self, **kwargs):
+            self.queries.append(kwargs)
+            return {"data": self.pages.pop(0), "meta": {"total_doc": 2}}
+
+    class DummyMaterials:
+        def __init__(self):
+            self.summary = DummySummary()
+
+    class DummyClient:
+        def __init__(self, api_key, **kwargs):
+            self.materials = DummyMaterials()
+
+        def close(self):
+            pass
+
+    monkeypatch.setattr("mp_helper.api.MPRester", DummyClient)
+    from mp_helper.materials import MaterialsSummarySearcher
+
+    with warnings.catch_warnings(record=True) as caught:
+        warnings.simplefilter("always")
+        searcher = MaterialsSummarySearcher()
+        chunks = list(searcher.iter_search_chunks(as_dict=True))
+
+    assert chunks == [[{"material_id": "mp-1"}], [{"material_id": "mp-2"}]]
+    assert searcher.mpr.materials.summary.queries == [
+        {
+            "criteria": {"_skip": 0},
+            "fields": None,
+            "chunk_size": 1000,
+            "num_chunks": 1,
+            "use_document_model": False,
+        },
+        {
+            "criteria": {"_skip": 1},
+            "fields": None,
+            "chunk_size": 1000,
+            "num_chunks": 1,
+            "use_document_model": False,
+        },
+        {
+            "criteria": {"_skip": 2},
+            "fields": None,
+            "chunk_size": 1000,
+            "num_chunks": 1,
+            "use_document_model": False,
+        },
+    ]
+
+    assert any("unbounded summary query" in str(w.message) for w in caught)
